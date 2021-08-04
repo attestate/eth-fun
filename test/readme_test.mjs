f754eccc7e6456a8892a7022a78a8438b3cf82c3
Tim Daubenschütz <tim@daubenschuetz.de>
2021-08-04 16:57:59 +0200

    Speed-up readme tests
    - Fixes #8

--- test/readme_test.mjs
+++ test/readme_test.mjs
@@ -3,23 +3,27 @@ import { readFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import { testmd } from "test-readme-md";
+import { matchBlocks, exec } from "test-readme-md";
 import test from "ava";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-test("run all examples in readme", async t => {
-  let content = readFileSync("./readme.md").toString();
-  content = content.replace(
-    new RegExp("eth-fun", "g"),
-    `file://${path.resolve(__dirname, "../src/index.mjs")}`
-  );
 
-  try {
-    await testmd("js", content);
-  } catch (err) {
-    console.log(err);
-    t.fail();
-  }
+let content = readFileSync("./readme.md").toString();
+content = content.replace(
+  new RegExp("eth-fun", "g"),
+  `file://${path.resolve(__dirname, "../src/index.mjs")}`
+);
+const blocks = matchBlocks("js", content);
 
-  t.pass();
-});
+for (let block of blocks) {
+  test(`run ${block.split("\n")[0]}`, async t => {
+    try {
+      await exec(block);
+    } catch (err) {
+      console.log(err);
+      t.fail();
+    }
+
+    t.pass();
+  });
+}
