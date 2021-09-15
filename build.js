a3b67b230f813de4df656a1c3f9fc3e137b61fa6
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2021-09-16 00:30:24 +0530

    fix build script added platform, target, format

--- build.js
+++ build.js
@@ -6,5 +6,8 @@ build({
   entryPoints: ['./src/index.js'],
   outfile: './cjs/index.js',
   external: [...Object.keys(json.dependencies), ...Object.keys(json.devDependencies)],
+  platform: 'node',
+  target: 'node14.16.1',
+  format: 'cjs',
   bundle: true,
 }).catch(() => process.exit(1))
