c88b2e2879fdde9694f47fcc4c924718c9649c8e
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2021-11-04 00:52:28 +0530

    added explanation for `Promise.allSettled`

--- examples/limit-requests/index.js
+++ examples/limit-requests/index.js
@@ -16,6 +16,14 @@ for (let i = 0; i < totalRequests; i++) {
   promises.push(pool(() => blockNumber(options)));
 }
 
+/*
+ * Promise.allSettled returns a promise that resolves after all of the given
+ * promises have either fulfilled or rejected. It retuens an array of objects
+ * that each describes the outcome of each promise.
+ *
+ * Unlike Promise.all it doesn't get rejected if one of the input promise is rejected.
+ * More info https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
+ */
 const results = await Promise.allSettled(promises);
 const countFullfiled = results.filter(
   (result) => result.status === "fulfilled"
